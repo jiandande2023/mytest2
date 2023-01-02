236e3e6e84519e2fa8acaf3bd24b1bf91ab20c11
root <1026967935@qq.com>
2023-01-02 19:57:46 +0800

    初始化项目

---
+++ git_day01_demo/src/main/java/com/itheima/domain/Customer.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName Customer
+ * @Description TODO
+ * @Author ly
+ * @Company 深圳黑马程序员
+ * @Date 2019/2/24 18:08
+ * @Version V1.0
+ */
+public class Customer {
+    private Integer id;
+}
---
+++ git_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,16 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author ly
+ * @Company 深圳黑马程序员
+ * @Date 2019/2/24 17:55
+ * @Version V1.0
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String houtai; // 后台人员开发
+    private String qianduan; // 前端人员开发
+}
